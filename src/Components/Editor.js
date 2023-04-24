4dfa97cccda4d83776880d9fe1a9526e1fd04020
azamdzobair <azamdzobair@gmail.com>
2023-04-24 14:12:24 +0600

    add Icon

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -4,9 +4,9 @@ const Editor = () => {
     return (
         <div>
             <div>
-                <div>
-                    <div className='flex'>
-                        <p>/</p>
+                <div className='bg-gray-950 '>
+                    <div className='flex px-3 py-3'>
+                        <p className='bg-red-600 rounded-md flex justify-center w-5 h-5 pb-0.5 mr-1.5'>/</p>
                         <h3>HTML</h3>
                     </div>
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
+        <div className=' bg-black h-10'>
             <h1>Logo</h1>
         </div>
     );
